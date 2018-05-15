0f61bb128f9dc850389ed7692f83bf878220dc64
Joshua Herman <zitterbewegung@users.noreply.github.com>
2018-05-15 10:08:06 -0500

    added vorpal token solidity code

---
+++ contracts/vorpalToken.sol
@@ -0,0 +1,9 @@
+pragma solidity 0.4.19;
+
+import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
+
+contract GustavoCoin is MintableToken {
+    string public name = "Vorpal Coin";
+    string public symbol = "VRP";
+    uint8 public decimals = 18;
+}
