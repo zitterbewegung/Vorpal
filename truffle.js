5d392bb9b4b7946f4ac3675546621589cc0beb57
Joshua Herman <30265+zitterbewegung@users.noreply.github.com>
2018-05-20 08:23:58 -0500

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -12,13 +12,10 @@ module.exports = {
 	    network_id: "*"
 	    
 	},
-    	rinkeby: {
-      		host: "localhost", // Connect to geth on the specified
-      		port: 8545,
-      		from: "0xbaB939d600e8CE222eD42B05D714d095fb4D690b", // default address to use for any transaction Truffle makes during migrations
-      		network_id: 4,
-      		gas: 4612388 // Gas limit used for deploys
-    	},
+     rinkeby: {
+      provider: new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/"+infura_apikey),
+      network_id: 3
+    },
      ropsten: {
       provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infura_apikey),
       network_id: 3
