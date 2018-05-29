41683bdea7c657c793b87d50e403ff13fe8ac685
Joshua Herman <30265+zitterbewegung@users.noreply.github.com>
2018-05-29 18:55:54 -0500

    Update index.js

--- front/src/index.js
+++ front/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 import { DrizzleProvider } from "drizzle-react";
 
 // Import contract
